715de33422b60802f3264c63473a6bb7c39a86ef
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-04-02 08:31:29 -0400

    Initial Commit

---
+++ TableSearch2014/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableSearch2014
+//
+//  Created by Jay Versluis on 02/04/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TableSearch2014Tests/TableSearch2014Tests.m
@@ -0,0 +1,34 @@
+//
+//  TableSearch2014Tests.m
+//  TableSearch2014Tests
+//
+//  Created by Jay Versluis on 02/04/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableSearch2014Tests : XCTestCase
+
+@end
+
+@implementation TableSearch2014Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
